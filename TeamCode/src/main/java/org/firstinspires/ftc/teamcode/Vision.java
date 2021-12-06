7b8bbb9423a535e6647ef23008a0b0e8c81dd5ef
Aditya Mangalampalli <aditya.mangalampalli@gmail.com>
2021-12-05 21:53:31 -0800

    Fixing git

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
@@ -22,34 +22,44 @@ public class Vision extends LinearOpMode
     {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
-
         phoneCam.setPipeline(new NativePipeline());
         phoneCam.setViewportRenderer(OpenCvCamera.ViewportRenderer.GPU_ACCELERATED);
+        phoneCam.setViewportRenderingPolicy(OpenCvCamera.ViewportRenderingPolicy.OPTIMIZE_VIEW);
         phoneCam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
         {
             @Override
-            public void onOpened() {
-                phoneCam.startStreaming(1280, 960, OpenCvCameraRotation.UPRIGHT);
+            public void onOpened()
+            {
+                phoneCam.startStreaming(320, 240, OpenCvCameraRotation.SIDEWAYS_LEFT);
             }
 
             @Override
-            public void onError(int errorCode) {
+            public void onError(int errorCode)
+            {
 
             }
         });
 
         telemetry.addLine("Waiting for start");
         telemetry.update();
+
+        /*
+         * Wait for the user to press start on the Driver Station
+         */
         waitForStart();
 
         while (opModeIsActive())
         {
+            /*
+             * Send some stats to the telemetry
+             */
             telemetry.addData("Frame Count", phoneCam.getFrameCount());
             telemetry.addData("FPS", String.format("%.2f", phoneCam.getFps()));
             telemetry.addData("Total frame time ms", phoneCam.getTotalFrameTimeMs());
             telemetry.addData("Pipeline time ms", phoneCam.getPipelineTimeMs());
             telemetry.addData("Overhead time ms", phoneCam.getOverheadTimeMs());
             telemetry.addData("Theoretical max FPS", phoneCam.getCurrentPipelineMaxFps());
+            telemetry.addData("Position", NativePipeline.getPosition());
             telemetry.update();
         }
     }
